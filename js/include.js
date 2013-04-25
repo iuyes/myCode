40193ddc5ea6b12dc4aacd2e18ae5c8b5c77492a
liushuai <liushuai_04@163.com>
2013-04-25 11:28:44 +0800

    动态加载js，css的jq扩展

---
+++ js/include.js
@@ -0,0 +1,23 @@
+/**
+ * jquery动态加载js，css的扩展
+ *
+ */
+$.extend({
+    includePath: '',
+    include: function(file)
+    {
+        var files = typeof file == "string" ? [file] : file;
+        for (var i = 0; i < files.length; i++)
+        {
+            var name = files[i].replace(/^\s|\s$/g, "");
+            var att = name.split('.');
+            var ext = att[att.length - 1].toLowerCase();
+            var isCSS = ext == "css";
+            var tag = isCSS ? "link" : "script";
+            var attr = isCSS ? " type='text/css' rel='stylesheet' " : " language='javascript' type='text/javascript' ";
+            var link = (isCSS ? "href" : "src") + "='" + $.includePath + name + "'";
+            if ($(tag + "[" + link + "]").length == 0) document.write("<" + tag + attr + link + "></" + tag + ">");
+        }
+    }
+});
+$.include(['http://image.esunny.com/script/jquery.divbox.js','/css/pop_win.css']);
